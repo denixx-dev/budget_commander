2204feb0e6632ada28a493336d175b7d7fa60a7a
denixx-dev <denissm2002@yandex.ru>
2024-03-06 21:16:04 +0300

    developed stable registration

--- src/main/java/com/denissmirnov/budgetcommander/controllers/RegisterController.java
+++ src/main/java/com/denissmirnov/budgetcommander/controllers/RegisterController.java
@@ -1,11 +1,12 @@
 package com.denissmirnov.budgetcommander.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @CrossOrigin("*")
 public class RegisterController {
