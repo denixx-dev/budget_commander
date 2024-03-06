550846201aeb58fef098b0bc6f9489af5fb311ef
denixx-dev <denissm2002@yandex.ru>
2024-03-06 17:32:01 +0300

    added LoginController

--- src/main/java/com/denissmirnov/budgetcommander/controllers/LoginController.java
+++ src/main/java/com/denissmirnov/budgetcommander/controllers/LoginController.java
@@ -1,16 +1,21 @@
 package com.denissmirnov.budgetcommander.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
+@Slf4j
+@Controller
+@RequestMapping("/auth/login")
 @CrossOrigin("*")
 public class LoginController {
     @GetMapping
     public String showLoginForm(){
         return "login";
     }
+
+
 }
